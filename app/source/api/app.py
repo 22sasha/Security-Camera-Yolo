0eec902b8ac950257e9a4dc63642dbc2ac53736a
22sasha <bearbearovich@gmail.com>
2024-12-05 15:02:18 +0300

    page123 v0.001

--- app/source/api/app.py
+++ app/source/api/app.py
@@ -1,11 +1,23 @@
 from fastapi import FastAPI
-# from api.task import router as task_router
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+from starlette.requests import Request
 
-app = FastAPI(
-    title="Video GUI"
-)
+app = FastAPI()
 
+templates = Jinja2Templates(directory="/templates")
 
-@app.get("/ping")
-async def root():
-    return "pong"
+# app.mount("/static", StaticFiles(directory="/static"), name="static")
+
+@app.get("/", response_class=HTMLResponse)
+async def read_page1(request: Request):
+    return templates.TemplateResponse("page1.html", {"request": request})
+
+@app.get("/page2", response_class=HTMLResponse)
+async def read_page2(request: Request):
+    return templates.TemplateResponse("page2.html", {"request": request})
+
+@app.get("/page3", response_class=HTMLResponse)
+async def read_page3(request: Request):
+    return templates.TemplateResponse("page3.html", {"request": request})
