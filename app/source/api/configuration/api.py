0c4f6414960ba508e2a769ca4cc1c225178e3eb6
22sasha <bearbearovich@gmail.com>
2025-01-16 11:29:43 +0300

    add pagination in configuration

--- app/source/api/configuration/api.py
+++ app/source/api/configuration/api.py
@@ -4,6 +4,8 @@ from fastapi.templating import Jinja2Templates
 from services.camera_config import CameraConfigServiceInterface, get_camera_config_service
 from shared.schemas.camera_config import params as CameraConfigParams
 from shared.schemas.utils import PaginationParams, SortParams, SortOrder
+from math import ceil
+
 
 router = APIRouter(
     prefix="",
@@ -13,13 +15,22 @@ templates = Jinja2Templates(directory="templates")
 
 @router.get("/configuration", response_class=HTMLResponse)
 async def read_configuration(request: Request,
+                             page: int = 1,
+                             per_page: int = 20,
                              service: CameraConfigServiceInterface = Depends(get_camera_config_service)):
-    pagination_params = PaginationParams(page=1, perPage=20)
+    pagination_params = PaginationParams(page=page, perPage=per_page)
     sort_params = SortParams(sortBy="id", sortOrder=SortOrder.Desc)
     params = CameraConfigParams.List(pagination=pagination_params, sort=sort_params)
     configs = await service.list(params=params)
-    response = templates.TemplateResponse(
-        "configuration.html", 
-        {"request": request, "configs": configs.items}
+
+    total_pages = ceil(configs.total / per_page)
+
+    response = templates.TemplateResponse("configuration.html", 
+        {
+            "request": request, 
+            "configs": configs.items,
+            "total_pages": total_pages,
+            "current_page": page
+        }
     )
     return response
