41404f9a677bdad69f9ab87eac5575f29d933417
22sasha <bearbearovich@gmail.com>
2025-02-03 14:31:45 +0300

    depth v0.1

--- app/source/common/camera.py
+++ app/source/common/camera.py
@@ -5,6 +5,7 @@ import torch
 import threading
 from fastapi import HTTPException
 import time
+from collections import defaultdict, deque
 
 
 class Camera:
@@ -28,6 +29,9 @@ class Camera:
         self.url = url
         self.camera_id = camera_id
         self.cap = self.__capture_video(url)
+
+        self.detect_depth = 60
+        self.track_history = defaultdict(lambda: deque(maxlen=self.detect_depth))
 
         self.thread = threading.Thread(target=self.__update_frame)
         self.thread.daemon = True
@@ -54,14 +58,39 @@ class Camera:
 
     def __process_prediction(self, results):
         detections = []
+        current_frame_tracks = set()
+
         for result in results:
             for detection in result.boxes.data:
                 if len(detection) >= 7:
                     x_min, y_min, x_max, y_max, track_id, confidence, class_id = detection[:7]
-                    detections.append({"class_id": int(class_id)})
+                    track_id = int(track_id)
+                    current_frame_tracks.add(track_id)
+
+                    self.track_history[track_id].append(1)
+
+                    if sum(self.track_history[track_id]) >= self.detect_depth:
+                        detections.append({
+                            # "track_id": track_id,
+                            "class_id": int(class_id),
+                            # "confidence": float(confidence),
+                            # "bbox": [float(x_min), float(y_min), float(x_max), float(y_max)]
+                        })
                 else:
                     x_min, y_min, x_max, y_max, confidence, class_id = detection[:6]
-                    detections.append({"class_id": int(class_id)})
+                    detections.append({
+                        "class_id": int(class_id),
+                        # "confidence": float(confidence),
+                        # "bbox": [float(x_min), float(y_min), float(x_max), float(y_max)]
+                    })
+
+        for track_id in list(self.track_history.keys()):
+            if track_id not in current_frame_tracks:
+                self.track_history[track_id].append(0)
+
+            if sum(self.track_history[track_id]) == 0:
+                del self.track_history[track_id]
+
         return detections
 
     def __update_frame(self):
@@ -75,7 +104,15 @@ class Camera:
         self.stop()
 
     def get_frame(self):
-        results = self.model.track(source=self.image, conf=self.confidence, verbose=False)
+        results = self.model.track(source=self.image, 
+                                   conf=self.confidence,
+                                   verbose=False,
+                                #    iou=0.3,
+                                   device="cuda:0",
+                                #    max_age=30,
+                                #    min_hits=4,
+                            )
+
         detections = self.__process_prediction(results)
         annotated_image = results[0].plot()
         annotated_image = cv2.resize(annotated_image, (self.width, self.height))
